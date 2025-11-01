7b57cef66a87719b38a9fd8801f0fa30590e2390
VedantSingh-dev <singhvedant092@gmail.com>
2025-11-01 21:12:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+let obj = {};
+async function stats() {
+    const id = document.querySelector('input').value;
+    const apiUrl = `https://lc-companion-backend-api.vercel.app/stats/${id}`;
+    const response = await fetch(apiUrl);
+    const json = await response.json();
+    obj = json;
+    ques_cnt()
+}
+
+
+function ques_cnt() {
+    document.querySelector('.total').innerHTML = obj.Questions_Solved.total;
+    document.querySelector('.easy').innerHTML = obj.Questions_Solved.easy;
+    document.querySelector('.medium').innerHTML = obj.Questions_Solved.medium;
+    document.querySelector('.hard').innerHTML = obj.Questions_Solved.hard;
+    contest();
+}
+
+
+function contest() {
+    document.querySelector('.rating').innerHTML = obj.Contest_History.overall_rating.toFixed(2);
+    const contestList = document.querySelector('.contest-list');
+    contestList.innerHTML = '';
+
+    const arr = obj.Contest_History.contests;
+    arr.forEach(e => {
+        let ele = document.createElement("div");
+        ele.innerHTML = `<div><strong>${e.contest_name}</strong><span>${e.questions_solved}/4</span></div>`;
+        contestList.appendChild(ele.firstChild);
+    });
+    topics();
+}
+
+
+function topics() {
+    const topic = obj.Topicwise_Question_Solved;
+    const topicTags = document.querySelector('.topic-tags');
+    topicTags.innerHTML = '';
+
+    for (let key in topic) {
+        let ele = document.createElement("div");
+        ele.innerHTML = `<span>${key}: ${topic[key]}</span>`;
+        topicTags.appendChild(ele.firstChild);
+    }
+    last_submissions();
+}
+
+function last_submissions() {
+    const submissions = obj.Last_20_Accepted_Submissions;
+    const submissionList = document.querySelector('.submission-list');
+    submissionList.innerHTML = '';
+
+    for (let key in submissions) {
+        let ele = document.createElement("div");
+        ele.innerHTML = `<div>${key}: ${submissions[key]}</div>`;
+        submissionList.appendChild(ele.firstChild);
+    }
+}
+
+async function roadmap() {
+    const apiUrl = `https://lc-companion-backend-api.vercel.app/roadmap/${JSON.stringify(obj)}`;
+    const response = await fetch(apiUrl);
+    const html = await response.text();
+    const newTab = window.open();
+    newTab.document.write(html);
+    newTab.document.close();
+}
